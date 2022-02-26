c4c4e952d8762d951638a083fb8dafab8b76e8f1
wtsiwon <siwon4727@gmail.com>
2022-02-26 12:17:40 +0900

    난분명히 한게 없는뎁..

--- SMG2022/Assets/Scripts/StartButton.cs
+++ SMG2022/Assets/Scripts/StartButton.cs
@@ -1,12 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class StartButton : MonoBehaviour
 {
+    public bool Startbutton;
     public void StartButtonClick()
     {
-
+        transform.DOMoveY(-625, 0.9f).SetEase(Ease.InBack);
+        Startbutton = true;
     }
+
 }
 
