046a0f50bc8508fb163908e8c9e24294f2201a57
sungbum05 <sblim05@naver.com>
2022-02-26 11:36:11 +0900

    RocketScene

--- SMG2022/Assets/SungBum/Scripts/GameMgr.cs
+++ SMG2022/Assets/SungBum/Scripts/GameMgr.cs
@@ -6,7 +6,9 @@ public class GameMgr : SingletonMono<GameMgr>
 {
     public Vector3 PlayerPos;
 
-    public int Stage = 0;
+    public int Stage = 1;
 
     public int EventNumber = 0;
+
+    public List<bool> PartCheck;
 }
---
+++ SMG2022/Assets/SungBum/Scripts/RocketSceneMgr.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RocketSceneMgr : MonoBehaviour
+{
+    public List<StagePart> StageRocketPart;
+
+    public GameObject FireBtn;
+
+    private int StageNum = GameMgr.In.Stage - 1;
+    
+    // Start is called before the first frame update 
+    void Start() 
+    {
+        TypeSetting();
+        ActiveImg();
+
+        //RocketImg.texture = StageRocketPart[0].RocketPart[0];
+    }
+
+    void TypeSetting()
+    {
+        StageRocketPart[StageNum].StageType.SetActive(true);
+    }
+
+    void ActiveImg()
+    {
+        int FireCheck = 0;
+
+        if(StageRocketPart[StageNum].StageType.name == "Type1")
+        {
+            if(GameMgr.In.PartCheck[0] == true)
+            {
+                StageRocketPart[StageNum].Part[0].texture = StageRocketPart[StageNum].RocketPart[0];
+                FireCheck += 1;
+            }
+
+            if (GameMgr.In.PartCheck[1] == true)
+            {
+                StageRocketPart[StageNum].Part[1].texture = StageRocketPart[StageNum].RocketPart[1];
+                FireCheck += 1;
+            }
+
+            if(FireCheck == 2)
+            {
+                FireBtn.SetActive(true);
+            }
+        }
+
+        else if (StageRocketPart[StageNum].StageType.name == "Type2")
+        {
+            if (GameMgr.In.PartCheck[0] == true)
+            {
+                StageRocketPart[StageNum].Part[0].texture = StageRocketPart[StageNum].RocketPart[0];
+                FireCheck += 1;
+            }
+
+            if (GameMgr.In.PartCheck[1] == true)
+            {
+                StageRocketPart[StageNum].Part[1].texture = StageRocketPart[StageNum].RocketPart[1];
+                FireCheck += 1;
+            }
+
+            if (GameMgr.In.PartCheck[2] == true)
+            {
+                StageRocketPart[StageNum].Part[2].texture = StageRocketPart[StageNum].RocketPart[2];
+                FireCheck += 1;
+            }
+
+            if (FireCheck == 3)
+            {
+                FireBtn.SetActive(true);
+            }
+        }
+    }
+}
