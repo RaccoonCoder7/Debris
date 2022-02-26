cc44cdf2d357b8e4d7778b543c9cf5be2e53daba
wtsiwon <siwon4727@gmail.com>
2022-02-27 01:16:05 +0900

    오류없앰...

--- SMG2022/Assets/Scripts/DataSave.cs
+++ SMG2022/Assets/Scripts/DataSave.cs
@@ -20,11 +20,11 @@ public class DataSave : MonoBehaviour
     {
         PlayerPrefs.SetFloat("PlayerX",Player.transform.position.x);
         PlayerPrefs.SetFloat("PlayerY",Player.transform.position.y);
-        PlayerPrefs.SetInt("ClearStage", GameMgr.clearedEvent[0].Toint16());
-        PlayerPrefs.SetInt("ClearStage", GameMgr.clearedEvent[1].Toint16());
-        PlayerPrefs.SetInt("ClearStage", GameMgr.clearedEvent[2].Toint16());
-        PlayerPrefs.SetInt("ClearStage", GameMgr.clearedEvent[3].Toint16());
-        PlayerPrefs.SetInt("ClearStage", GameMgr.clearedEvent[4].Toint16());
+        //PlayerPrefs.SetInt("ClearStage", GameMgr.clearedEvent[0].Toint16());
+        //PlayerPrefs.SetInt("ClearStage", GameMgr.clearedEvent[1].Toint16());
+        //PlayerPrefs.SetInt("ClearStage", GameMgr.clearedEvent[2].Toint16());
+        //PlayerPrefs.SetInt("ClearStage", GameMgr.clearedEvent[3].Toint16());
+        //PlayerPrefs.SetInt("ClearStage", GameMgr.clearedEvent[4].Toint16());
     }
     public void GameLoad()
     {
